d637a82fdca6d1ce52a47817ba08feb9f87c69c5
Dwi Darma <dwikbtn15@gmail.com>
2021-03-21 21:53:31 +0800

    adding new attribute

--- colorChanger/colorChanger.js
+++ colorChanger/colorChanger.js
@@ -2,6 +2,25 @@ const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.querySelector("body");
 const cssCode = document.querySelector(".css-code");
+const addColor = document.querySelector(".add-color");
+const section = document.querySelector("section.container");
+
+//add Color
+addColor.addEventListener("click", function () {
+  const inputClr2 = document.createElement("input");
+  // inputClr2.setAttribute("type", "color");
+  function setAttributes(el, attrs) {
+    for (const key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+  }
+  setAttributes(inputClr2, {
+    type: "color",
+    class: "color2",
+    value: "#ffff66",
+  });
+  console.log(inputClr2);
+});
 
 //change color
 changeColor = () => {

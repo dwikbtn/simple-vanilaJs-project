97568a18fc53561fed1269ab4264012a1b6b7678
Dwi Darma <dwikbtn15@gmail.com>
2021-03-24 16:39:46 +0800

    finish the project

--- projects/rock-paper-scissor/rps.js
+++ projects/rock-paper-scissor/rps.js
@@ -13,11 +13,52 @@ const getResult = (comp, player) => {
   if (player == "scissor") return comp == "rock" ? "KALAH" : "MENANG!";
 };
 
-const pRock = document.querySelector(".rock");
-pRock.addEventListener("click", () => {
-  const botPick = getBotResult();
-  const playerPick = pRock.className;
+const loading = () => {
+  const spinnerImg = document.querySelector(".img-komputer");
+  const picList = ["rock", "paper", "scissor"];
+  let i = 0;
+  const waktuMulai = new Date().getTime();
+  setInterval(function () {
+    if (new Date().getTime() - waktuMulai > 1000) {
+      clearInterval;
+      return;
+    }
+    spinnerImg.setAttribute("src", "img/" + picList[i++] + ".png");
+    if (i == picList.length) i = 0;
+  }, 100);
+};
+//pick rock
+// const pRock = document.querySelector(".rock");
+// pRock.addEventListener("click", () => {
+//   const botPick = getBotResult();
+//   const playerPick = pRock.className;
 
-  const result = getResult(botPick, playerPick);
-  console.log(result);
+//   const result = getResult(botPick, playerPick);
+
+//   //change computer image
+//   const pComputerImage = document.querySelector(".img-komputer");
+//   pComputerImage.setAttribute("src", "img/" + botPick + ".png");
+
+//   const printResult = document.querySelector(".info");
+//   printResult.innerHTML = result;
+// });
+
+const pickOne = document.querySelectorAll("li img");
+pickOne.forEach((image) => {
+  image.addEventListener("click", () => {
+    const botPick = getBotResult();
+    const playerPick = image.className;
+
+    const result = getResult(botPick, playerPick);
+
+    loading();
+    //change computer image
+    setTimeout(() => {
+      const pComputerImage = document.querySelector(".img-komputer");
+      pComputerImage.setAttribute("src", "img/" + botPick + ".png");
+
+      const printResult = document.querySelector(".info");
+      printResult.innerHTML = result;
+    }, 1000);
+  });
 });
